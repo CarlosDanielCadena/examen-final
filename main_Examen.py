918078b81ef432737709ec772b37446f8a902b89
CarlosDanielCadena <69741273+CarlosDanielCadena@users.noreply.github.com>
2020-11-24 09:58:45 -0500

    Update main_Examen.py

--- main_Examen.py
+++ main_Examen.py
@@ -4,5 +4,6 @@ if __name__ == "__main__":       # Main
     path = input("Por favor ingrese la ruta de la imagen: ")    # Ingreso de la ruta de la imagen
     imagen = bandera(path)
     imagen.colores()
+    imagen.orientacion()
     imagen.porcentaje()
-    imagen.orientacion()    
+
